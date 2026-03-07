e4b79ac645e13004a6036ec5219cd184b35e8109
Jennifer Wagner <49212830+jenniferwagner18@users.noreply.github.com>
2026-03-07 11:42:51 -0700

    Update course-access.sql

--- course-access.sql
+++ course-access.sql
@@ -1,6 +1,26 @@
 /* Course access via browser and Pulse app (one row each day that student accesses course per source). 
    Remove both DATE functions and DISTINCT for all Pulse sessions rather than one per day. */
-
+WITH ca AS (
+   SELECT
+      orgunitid,
+      userid,
+      DATE(CAST(dayaccessed AS timestamp)) AS dayaccessed, /* logged as local org-level timezone */
+      'Browser' AS source
+   FROM
+       brightspace_data_sets_[your_schema_id].courseaccess_10_9_5
+   WHERE 
+       dayaccessed IS NOT NULL
+   UNION
+   SELECT DISTINCT
+       orgunitid,
+       userid,
+       DATE(CAST(REPLACE(timestamp, ' UTC', '') AS timestamp) AT TIME ZONE 'UTC' AT TIME ZONE 'America/New_York') AS dayaccessed, /* logged in UTC; change to own timezone */
+       source
+   FROM
+       brightspace_data_sets_[your_schema_id].courseaccesslog_10_9_5
+   WHERE 
+       timestamp IS NOT NULL
+)
 SELECT
     access.orgunitid,
     courses.code,
@@ -10,27 +30,7 @@ SELECT
     users.lastname,
     access.dayaccessed,
     access.source
-FROM (
-SELECT
-    orgunitid,
-    userid,
-    DATE(CAST(dayaccessed AS timestamp)) AS dayaccessed, /* logged as local org-level timezone */
-    'Browser' AS source
-FROM
-    brightspace_data_sets_[your_schema_id].courseaccess_9_9_3
-WHERE 
-    dayaccessed IS NOT NULL
-UNION
-SELECT DISTINCT
-    orgunitid,
-    userid,
-    DATE(CAST(REPLACE(timestamp, ' UTC', '') AS timestamp) AT TIME ZONE 'UTC' AT TIME ZONE 'America/New_York') AS dayaccessed, /* logged in UTC; change to own timezone */
-    source
-FROM
-    brightspace_data_sets_[your_schema_id].courseaccesslog_9_9_3
-WHERE 
-    timestamp IS NOT NULL
-) AS access
+FROM access
 JOIN brightspace_data_sets_[your_schema_id].organizationalunits_9_9_3 AS courses 
     ON access.orgunitid = courses.orgunitid
 JOIN brightspace_data_sets_[your_schema_id].users_9_9_3 AS users    
